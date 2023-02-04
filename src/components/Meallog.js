409c7b1d64446fa52ecd7a895220167a5ac64596
Matt <matt@spectrebot.net>
2023-02-04 23:07:27 +0000

    miss date in meallogs

--- src/components/Meallog.js
+++ src/components/Meallog.js
@@ -3,7 +3,7 @@ import styles from '@/styles/Meallog.module.css'
 export default function Meallog(props) {
     return (
         <div className={styles.mealLog}>
-            <p>{props.mealDate}</p>
+            {/* <p>{props.mealDate}</p> */}
             <h3 className={styles.mealName}>{props.mealName}</h3>
             <p>{props.mealNotes}</p>
             <p>{props.mealHealthiness}</p>
