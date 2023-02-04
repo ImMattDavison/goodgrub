03a87d02d1daebfa8c8e0f907f47843fb468085d
Matt <matt@spectrebot.net>
2023-02-04 13:45:36 +0000

    update header imports

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,9 @@
 import styles from '../styles/Header.module.css'
 import { useState, useEffect } from 'react'
 import Link from 'next/link'
+import { supabase } from '../utils/supabase'
+import { createBrowserSupabaseClient, Session } from '@supabase/auth-helpers-nextjs'
+import { SessionContextProvider, useUser } from '@supabase/auth-helpers-react'
 
 export default function Header() {
 
@@ -36,6 +39,21 @@ export default function Header() {
         console.log(pageSize);
     }, [navOpen, pageSize]);
 
+    const login = async function signInWithGoogle() {
+        const { data, error } = await supabase.auth.signInWithOAuth({
+            provider: 'google',
+            options: {
+                scopes: 'openid profile email'
+            }
+        })
+    }
+
+    const signOut = async function signOut() {
+        const { error } = await supabase.auth.signOut()
+    }
+
+    console.log(useUser())
+
     return (
         <header className={`${styles.header}`}>
             <nav className={`${styles.nav} container`}>
@@ -55,6 +73,14 @@ export default function Header() {
                         <li className={styles.navItem}>
                             <Link className={styles.navLink} href="/">HOME</Link>
                         </li>
+                        {!useUser() ? 
+                            <li>
+                                <button className={styles.login} onClick={login}>Log In</button>
+                            </li> :
+                            <li>
+                                <Link className={styles.navLink} href="/dashboard">DASHBOARD</Link>
+                            </li>
+                        }
                     </ul>
                 </div>
             </nav>
