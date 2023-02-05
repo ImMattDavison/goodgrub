b0429bfaf89d9539a85750406f106c63cd750662
Matt <matt@spectrebot.net>
2023-02-05 09:34:09 +0000

    make data make sense

--- src/components/Meallog.js
+++ src/components/Meallog.js
@@ -1,13 +1,19 @@
 import styles from '@/styles/Meallog.module.css'
 
 export default function Meallog(props) {
+
+    console.log(props.mealMood)
+
     return (
+
         <div className={styles.mealLog}>
             {/* <p>{props.mealDate}</p> */}
             <h3 className={styles.mealName}>{props.mealName}</h3>
             <p>{props.mealNotes}</p>
-            <p>{props.mealHealthiness}</p>
-            <p>{props.mealMood}</p>
+            <div className={styles.mealStats}>
+                <p>{'Health Score Self Assessment: ' + props.mealHealthiness}</p>
+                <p>Feeling: {props.mealMood == 5 ? 'Great' : props.mealMood == 4 ? 'Good' : props.mealMood == 3 ? 'Okay' : props.mealMood == 2 ? 'Bad' : props.mealMood == 1 ? 'Terrible' : 'No Mood'}</p>
+            </div>
         </div>
     )
 }
