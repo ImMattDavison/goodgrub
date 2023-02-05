4c16d81a26b43d948979341358669143462784f3
Matt <matt@spectrebot.net>
2023-02-05 09:49:49 +0000

    update meal log presentation

--- src/components/Meallog.js
+++ src/components/Meallog.js
@@ -9,7 +9,10 @@ export default function Meallog(props) {
         <div className={styles.mealLog}>
             {/* <p>{props.mealDate}</p> */}
             <h3 className={styles.mealName}>{props.mealName}</h3>
-            <p>{props.mealNotes}</p>
+            <div className={styles.mealNotes}>
+                <h4>Meal Notes</h4>
+                <p>{props.mealNotes}</p>
+            </div>
             <div className={styles.mealStats}>
                 <p>{'Health Score Self Assessment: ' + props.mealHealthiness}</p>
                 <p>Feeling: {props.mealMood == 5 ? 'Great' : props.mealMood == 4 ? 'Good' : props.mealMood == 3 ? 'Okay' : props.mealMood == 2 ? 'Bad' : props.mealMood == 1 ? 'Terrible' : 'No Mood'}</p>
